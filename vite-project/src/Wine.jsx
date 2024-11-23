9b34d2be559b58b80c188ed3e81202891be07402
azzimlii <ezimliinci@gmail.com>
2024-11-23 15:15:52 +0400

    task2 commit

--- vite-project/src/Wine.jsx
+++ vite-project/src/Wine.jsx
@@ -1,13 +1,20 @@
 import { Component } from "react";
 
-export default class Wine extends Component{
-    render(){
-        const { title, description, rating}=this.props;
+export default class Wine extends Component {
+    clicking = () => {
+        console.log("Button click!");
+    }
+    render() {
+        const { title, description, rating } = this.props;
         return (
             <div className="wine">
                 <h3>{title}</h3>
                 <p>{description}</p>
                 <p>{rating}</p>
+                <hr />
+                <button type="button" onClick={this.clicking}>
+                    Click
+                </button>
             </div>
         )
     }
